58aa9fbf92c34a6d98dbaa4bd50e59ccc0098205
Arpit <arpitvermaetw@gmail.com>
2024-04-22 19:05:18 +0530

    some change

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ import mongoose from "mongoose";
 const app = express();
 
 try {
-//   let conn = await mongoose.connect("mongodb+srv://arpit:Arpit@2003@cluster0.qvmmxj6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-  let conn =  mongoose.connect("mongodb://localhost:27017")
+// //   let conn = await mongoose.connect("mongodb+srv://arpit:Arpit@2003@cluster0.qvmmxj6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+//   let conn =  mongoose.connect("mongodb://localhost:27017")
 
-  console.log("Data Base Connected Successully")
+
+console.log("Data Base Connected Successully")
+let URI = "mongodb+srv://arpitvermaetw:xT3JaNpWGSabOHPC@innerdata.v6pwutf.mongodb.net/"
+
   
+  mongoose.connect(URI)
   
 } catch (error) {
   console.log("Not Connect" , error.message);
